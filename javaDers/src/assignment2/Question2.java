b6ccad45bab63276d634f5e6c46d38c57695f3b7
AbdurSert <abdursert@gmail.com>
2022-12-11 14:21:29 +0100

    last commit

---
+++ javaDers/src/assignment2/Question2.java
@@ -0,0 +1,15 @@
+package assignment2;
+
+public class Question2 {
+
+	public static void maain(String[] args) {
+		
+		String name = "Abdurrahman";
+		String lastName = "Sert";
+		String adress = "Elzenhagensingel 1625, 1022LA";
+		
+		System.out.println(name  + " " + lastName);
+		
+	}
+
+}

004966649d63fede5e70e75fa45e895a4e3ddfb3
AbdurSert <abdursert@gmail.com>
2022-12-14 17:30:43 +0100

    Commita

---
+++ javaDers/src/assignment3/Question9.java
@@ -0,0 +1,36 @@
+package assignment3;
+
+import java.util.Scanner;
+
+public class Question9 {
+
+	public static void main(String[] args) {
+
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Birinci sayiyi giriniz");
+		int sayi1 = scn.nextInt();
+
+		System.out.println("Ikinci sayiyi giriniz: ");
+		int sayi2 = scn.nextInt();
+
+		System.out.println("Ucuncu sayiyi giriniz: ");
+		int sayi3 = scn.nextInt();
+
+		if (sayi1 > sayi2 && sayi1 > sayi3) {
+
+			System.out.println("Giridiginiz sayilar arasindan en buyugu: " + sayi1);
+
+		}
+
+		else if (sayi2 > sayi1 && sayi2 > sayi3) {
+
+			System.out.println("\"Giridiginiz sayilar arasindan en buyugu: " + sayi2);
+		}
+
+		else {
+			System.out.println("Giridiginiz sayilar arasindan en buyugu: " + sayi3);
+		}
+
+	}
+
+}

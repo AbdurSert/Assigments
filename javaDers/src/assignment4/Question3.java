d32a717aa3bd5d7ba66a7c306b4342dad40f048a
AbdurSert <abdursert@gmail.com>
2022-12-18 15:45:40 +0100

    commit kac

--- javaDers/src/assignment4/Question3.java
+++ javaDers/src/assignment4/Question3.java
@@ -1,12 +1,37 @@
 package assignment4;
 
+import java.util.Scanner;
+
 public class Question3 {
-	
+
 	public static void main(String[] args) {
-		
-		
-		
-		
+
+		Scanner scn = new Scanner(System.in);
+
+		System.out.println("Lutfen isminizi giriniz: ");
+		String inpt = scn.nextLine();
+
+		System.out.println("Lutfen soyisminizi giriniz: ");
+		String inpt1 = scn.nextLine();
+
+		int lengthInpt = inpt.length();
+		int lengthInpt1 = inpt1.length();
+
+		if (lengthInpt > lengthInpt1) {
+
+			System.out.println("\n" + "Isminiz soyisminizden daha uzun.");
+
+		}
+
+		else if (lengthInpt == lengthInpt1) {
+
+			System.out.println("\n" + "Isminiz ile soysiminizin uzunlugu esit");
+		}
+
+		else {
+
+			System.out.println("\n" + "Soyisminiz isminizden daha uzun.");
+		}
 	}
 
 }
